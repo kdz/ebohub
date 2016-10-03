bcb5aedb7bc96ce1d2b8130ea443e10d990bddfd
kdz <kel.1303.github@dsouzaville.com>
2016-10-02 21:19:48 -0500

    add os.getenv for Twilio auth variables

--- src/twilio_setup.py
+++ src/twilio_setup.py
@@ -1,5 +1,7 @@
 from twilio.rest import TwilioRestClient
-TW_account = "my_twilio_account_number"
-TW_password = "my_twilio_auth_token"
-TW_phone = "+my_twilio_phone_number"  # format: "+15121234567"
+import os
+
+TW_account = os.getenv("TW_ACCOUNT_SID") #"my_twilio_account_number"
+TW_password = os.getenv("TW_AUTH_TOKEN") #"my_twilio_auth_token"
+TW_phone = os.getenv("TW_PHONE") #"+my_twilio_phone_number"  # format: "+15121234567"
 TW_client = TwilioRestClient(TW_account, TW_password)
